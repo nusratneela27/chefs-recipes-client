8ee81258f30caed83d12680d610a5d209c7e5347
neela <nusratneela27@gmail.com>
2023-05-03 04:27:59 +0600

    about section added

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -1,10 +1,28 @@
+import { Button } from 'react-bootstrap';
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import about from '../../../assets/about.jpg'
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About</h2>
-        </div>
+        <Container className='rounded bg-warning bg-opacity-25 mt-5 mb-5'>
+            <Row>
+                <Col md={6} className='p-5'>
+                    <div>
+                        <img src={about} alt="" height="400" weight="200" className='rounded' />
+                    </div>
+                </Col>
+                <Col md={6} className='p-5'>
+                    <h1 className='fw-bold'>About Us</h1>
+                    <p>
+                        Korea's cuisine is inspired by the agriculture and the seas surrounding it, namely the East China Sea and the Sea of Japan. South Korea's fertile plains are perfect for growing rice, a staple grain that accompanies most meals, and the seas are rich with tasty seafood like king crab and squid.
+                        <br />
+                        Ingredients and dishes vary by province. Many regional dishes have become national, and dishes that were once regional have proliferated in different variations across the country. Korean royal court cuisine once brought all of the unique regional specialties together for the royal family. Foods are regulated by Korean cultural etiquette.
+                    </p>
+                    <Button variant="secondary">Read More</Button>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
